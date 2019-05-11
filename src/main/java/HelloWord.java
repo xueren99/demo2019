502187ad042bb5ed85b28cfeb0d3e265eec4c799
xueren99 <xueren99@163.com>
2019-05-11 19:55:00 +0800

    hello world

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -1,6 +1,13 @@
+import com.alibaba.fastjson.JSONObject;
+
 public class HelloWord {
     public static void main(String[] args) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("word","hello json");
+
+
         System.out.println("hello word!");
+        System.out.println(jsonObject);
     }
 
 
